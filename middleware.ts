8a79ecc69f7aeab063d85123b8860da30de187ca
ikegwuonu <juliet.kegwuonu@gmail.com>
2025-04-23 11:01:46 +0100

    middleware

--- middleware.ts
+++ middleware.ts
@@ -1,1 +1,20 @@
-export { auth as middleware } from "@/auth";
+// export { auth as middleware } from "@/auth";
+// middleware.ts
+import { getToken } from "next-auth/jwt";
+import { NextResponse } from "next/server";
+import type { NextRequest } from "next/server";
+
+export async function middleware(request: NextRequest) {
+  const token = await getToken({ req: request });
+
+  if (!token) {
+    return NextResponse.redirect(new URL("/sign-in", request.url));
+  }
+
+  return NextResponse.next();
+}
+
+// (optional) Define which routes this applies to
+export const config = {
+  matcher: ["/admin,/books*", "/myprofile/:path*"], // example protected routes
+};
